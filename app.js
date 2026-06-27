85d7b4cf480b9ebddae2e887fa15a692254577a3
Nicolae N <35431660+niklaus17@users.noreply.github.com>
2026-06-27 19:10:46 +0300

    modificare link

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const API_URL =
-  "https://script.google.com/macros/s/AKfycby3AqyOCerO8f3hGXHA2-KH1wWWFxAKfbY7pTlW22cdB3Cjy-ELU2QpfHXI9TJtPptzwg/exec";
+  "https://script.google.com/macros/s/AKfycby9_tZRs9Q6gS8Z7RfFfVPSxRzSd6Ddbfmx0rXvjf-e6fkc_g-tMf42GTpg9UBuAQB5hg/exec";
 
 const tg = window.Telegram.WebApp;
 tg.expand();
